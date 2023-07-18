f7f6384087dd95f6a53d4d1c3d643829d2e5ab3c
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-07-18 10:53:24 +0300

    add:

---
+++ backend/shop/admin.py
@@ -0,0 +1,85 @@
+from django import forms
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+from ckeditor.widgets import CKEditorWidget
+
+from shop.models import (
+    ProductModel, 
+    ImageProductModel, 
+    ProductPropertyModel, 
+    OrderModel
+)
+
+
+
+class ProductAdminForm(forms.ModelForm):
+    """ Добавляем полю описания, виджет CKEditor """
+
+    description = forms.CharField(widget=CKEditorWidget(), label="Описание")
+    
+    class Meta:
+        model = ProductModel
+        fields = '__all__'
+
+
+class ProductImageInline(admin.TabularInline):
+    """ Вложенные изображения товара """
+
+    model = ImageProductModel
+    readonly_fields = ('preview', )
+
+    def preview(self, obj):
+        return mark_safe('<img style="margin-right: -20vh" src="/files/%s" alt="Нет изображения" width="80" height="80" />' % (obj.image))
+
+    preview.short_description = 'Изображение'
+    fieldsets = (
+        (None, {'fields': ('preview', 'image')}),
+        )
+    extra = 0
+
+
+class ProductPropertyInline(admin.TabularInline):
+    """ Вложенные свойства товара """
+
+    model = ProductPropertyModel
+    extra = 0
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+    """ Страница редактирования товара """
+
+    form = ProductAdminForm
+    list_display = ('id', 'title', 'last_update', 'activated')
+    list_display_links = ('id', 'title',)
+    list_editable = ('activated',)
+    search_fields = ('id', 'name',)
+    readonly_fields = ('last_update',)
+    inlines = (ProductImageInline, ProductPropertyInline)
+    fieldsets = (
+        (None, {'fields': (('title','activated'),('description', ),('last_update', ),)}),
+    )
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """ Отображение заказов в админке """
+    
+
+    def preview(self, obj):
+        return mark_safe('<img style="" src="/files/%s" alt="Нет изображения" width="auto" height="120" />' % (obj.image))
+
+    preview.short_description = ''
+    readonly_fields = ('preview', 'client_name', 'contact_data', 'created_data', 'comment', 'products', 'image',)
+    list_display = ('id', 'client_name', 'contact_data', 'created_data')
+    list_display_links = ('id', 'client_name',)
+
+    fieldsets = (
+        ('Данные клиента', {'fields': (('client_name','contact_data'),('comment', ),)}),
+        ('Данные заказа', {'fields': (('products',),)}),
+        ('Кастомный заказ', {'fields': (( 'image', 'preview',),)}),
+    )
+
+
+# Регистрация моделей в админке
+admin.site.register(ProductModel, ProductAdmin)
+admin.site.register(OrderModel, OrderAdmin)
---
+++ backend/shop/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+from django_resized import ResizedImageField
+
+
+class ProductModel(models.Model):
+    """ Модель товара """
+
+    activated = models.BooleanField(verbose_name='Показывать на сайте', default=True)
+    title = models.CharField(verbose_name='Название товара', max_length=250)
+    description = models.TextField(verbose_name="Описание товара", max_length=5000)
+
+    last_update = models.DateTimeField(verbose_name="Последнее обновление", auto_now=True)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ['-last_update',]
+
+    def __str__(self) -> str:
+        return str(self.title)
+    
+
+class ImageProductModel(models.Model):
+    """ Модель изображения товара """
+
+    image = ResizedImageField(
+        size = [640, 640],
+        verbose_name="",
+        crop = ['middle', 'center'],
+        upload_to='product/images/',
+        help_text="Изображение товара",
+        quality=100,
+        default='product/images/default.webp',
+        force_format='WEBP',
+    )
+
+    class Meta:
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображения товара'
+
+    product = models.ForeignKey(ProductModel, related_name="product_image", verbose_name="Изображение", on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return 'Изображение'
+    
+
+class ProductPropertyModel(models.Model):
+    """ Модель свойств товара """
+
+    name = models.CharField(verbose_name='Название свойства', max_length=250)
+    value = models.CharField(verbose_name='Значение свойства', max_length=200)
+    product = models.ForeignKey(ProductModel, related_name="product_property", verbose_name="Свойство", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Свойство товара'
+        verbose_name_plural = 'Свойства товара'
+
+    def __str__(self) -> str:
+        return f'{ self.name }: { self.value }'
+    
+
+
+class OrderModel(models.Model):
+    """ Модель заказа товара """
+
+    client_name = models.CharField(verbose_name='Клиент', max_length=300)
+    contact_data = models.CharField(verbose_name='Контакты клиента', max_length=300)
+    comment = models.TextField(verbose_name='Комментарий к заказу', max_length=1000)
+
+    image = models.ImageField(verbose_name='Изображение',upload_to='order/images/', help_text='Если кастомный заказ', null=True, blank=True)
+    products = models.JSONField(verbose_name='Заказанные товары', null=True, blank=True)
+
+    created_data = models.DateTimeField(verbose_name="Дата создания", auto_now=True)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self) -> str:
+        return f'Клиент: { self.client_name }'
