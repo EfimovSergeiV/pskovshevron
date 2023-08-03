f8e5bf6bde30981904e12b4a3b6e158cca739eae
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-08-03 08:30:51 +0300

    add:

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLayout>
-    <NuxtLoadingIndicator />
+    <NuxtLoadingIndicator :color="'repeating-linear-gradient(to right,#0ea5e9 0% ,#075985 100%)'" :height="2"/>
     <NuxtPage />
   </NuxtLayout>
 </template>
