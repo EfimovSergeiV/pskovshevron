caa5116aa1da1aa706b520a97201a4de5a327d20
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-08-09 09:43:43 +0300

    add:

--- backend/main/utils.py
+++ backend/main/utils.py
@@ -6,4 +6,5 @@ bot = Bot(token=settings.MAILBOT_TOKKEN)
 
 
 def send_alert_to_agent(msg):
-    bot.send_text(chat_id=settings.MAILBOT_CONTACTS, text=msg, parse_mode="HTML")
+    for contact in settings.MAILBOT_CONTACTS:
+        bot.send_text(chat_id=contact, text=msg, parse_mode="HTML")
